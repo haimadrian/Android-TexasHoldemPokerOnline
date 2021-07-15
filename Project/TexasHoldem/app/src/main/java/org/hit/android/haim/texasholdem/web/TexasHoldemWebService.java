adb14557eef58fe305246cf0901c8b547a4413d1
Haim Adrian <haimadrian@gmail.com>
2021-07-15 10:00:17 +0300

    Backend URL

--- Project/TexasHoldem/app/src/main/java/org/hit/android/haim/texasholdem/web/TexasHoldemWebService.java
+++ Project/TexasHoldem/app/src/main/java/org/hit/android/haim/texasholdem/web/TexasHoldemWebService.java
@@ -40,7 +40,7 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
  */
 public class TexasHoldemWebService {
     private static final String LOGGER = TexasHoldemWebService.class.getSimpleName();
-    private static final String BACKEND_URL = "https://ec2-34-224-135-87.compute-1.amazonaws.com:8443"; // EC2
+    private static final String BACKEND_URL = "********"; // EC2
 
     /** @see UserService */
     @Getter
